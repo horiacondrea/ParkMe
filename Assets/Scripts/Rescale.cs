20d57ea88a14bc22c5470f8578fe4586e82e5f2d
Condrea Horatiu <c.horias3@gmail.com>
2020-04-04 22:52:11 +0300

    Fix menu text for various resolutions

---
+++ Assets/Scripts/Rescale.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rescale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResizeSpriteToScreen();
+    }
+
+    void ResizeSpriteToScreen()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+        transform.localScale = new Vector3(1, 1, 1);
+
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        transform.localScale = new Vector3(worldScreenWidth / width, worldScreenHeight / height, 1);
+    }
+}
